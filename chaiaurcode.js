67ca567ce20cafbb09273034408909c86e153262
Sudhanshu jha <116909414+SudhanshuDTU@users.noreply.github.com>
2023-11-13 17:34:15 +0530

    Add files via upload

---
+++ chaiaurcode.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach((item, index) => {
+  item.addEventListener('click', (e) => {
+    switch (e.target.id) {
+      case 'grey':
+        body.style.backgroundColor = 'grey';
+        break;
+      case 'white':
+        body.style.backgroundColor = 'white';
+        break;
+      case 'blue':
+        body.style.backgroundColor = 'blue';
+        break;
+      case 'yellow':
+        body.style.backgroundColor = 'yellow';
+        break;
+      default:
+        body.style.backgroundColor = 'white';
+    }
+  });
+});
